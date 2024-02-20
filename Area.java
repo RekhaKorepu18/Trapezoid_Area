0cd9d2c8ef19dca01b9c71e9ee32ae2c903b4821
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-20 17:05:00 +0530

    Added two values succesfully

---
+++ Area.java
@@ -0,0 +1,11 @@
+class Area{
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        int a=sc.nextInt();
+        System.out.println("Enter the value of b:");
+        int b=sc.nextInt();
+        int addition=a+b;
+    }       
+}
