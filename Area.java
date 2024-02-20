198a3d560e79d5df0f13237f1267e2c64c9769c8
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-20 17:28:02 +0530

    multiplication logic file added

---
+++ Area.java
@@ -0,0 +1,14 @@
+class Area{
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        
+        
+        int h=sc.nextInt();
+        int divByTwo=sc.nextInt();
+        int result=h*divByTwo;
+        System.out.println("The Area of Trapezoid is",Result);
+    }
+        
+}
