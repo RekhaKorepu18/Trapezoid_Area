d5f72a47d33faad0356f99bca79f4a1b18e9de86
Admin <admin@Admins-MacBook-Pro-10.local>
2024-02-20 17:14:26 +0530

    Sum is divided by 2

---
+++ Area.java
@@ -0,0 +1,12 @@
+class Area{
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        
+        
+        int addition=sc.nextInt();
+        int divByTwo=addition/2;
+     }   
+}
+
